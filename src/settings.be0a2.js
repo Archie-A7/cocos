796234bcc4c535ca6f9ac34aaf989d9bd248a1c5
Archie0125 <smhh0x0@gmail.com>
2021-08-26 16:10:33 +0800

    poker

---
+++ src/settings.be0a2.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Logout.fire",orientation:"",jsList:[],bundleVers:{internal:"45e63",main:"4d9bb"}};
